2039b8021bc06e1143aded8a1e5b250b9fad9194
Krzysztof Rodak <krodak.konta@gmail.com>
2017-02-17 16:39:42 +0100

    19 - Adding Fakery to main target to generate mock data

--- Pods/Target Support Files/Pods-VyrlTests/Pods-VyrlTests-frameworks.sh
+++ Pods/Target Support Files/Pods-VyrlTests/Pods-VyrlTests-frameworks.sh
@@ -89,12 +89,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/FBSnapshotTestCase/FBSnapshotTestCase.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Fakery/Fakery.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/FBSnapshotTestCase/FBSnapshotTestCase.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/FBSnapshotTestCase/FBSnapshotTestCase.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Fakery/Fakery.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/FBSnapshotTestCase/FBSnapshotTestCase.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
