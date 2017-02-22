bbadd4f9faa16b566dcb51e3fcc55ea77ffd6fa7
Krzysztof Rodak <krodak.konta@gmail.com>
2017-02-22 11:48:25 +0100

    63 - Added APIConfiguration to be able to load API endpoint form plist

---
+++ Vyrl/APIConfiguration.swift
@@ -0,0 +1,54 @@
+//
+//  Copyright © 2017 Vyrl. All rights reserved.
+//
+
+import Foundation
+
+enum ConfigurationMode: String, CustomStringConvertible {
+    case Staging
+    case Production
+
+    var description: String {
+        return self.rawValue
+    }
+}
+
+enum ConfigurationError: Error {
+    case noConfigurationFile
+    case noBaseURL
+}
+
+protocol APIConfigurationHaving {
+    var baseURL: URL { get }
+    var mode: ConfigurationMode { get }
+}
+
+final class APIConfiguration: APIConfigurationHaving {
+
+    private struct ConfigurationKeys {
+        static let configurationFile    = "Configuration"
+        static let baseURL              = "BaseURL"
+        static let defaultFileType      = "plist"
+    }
+
+    private let configuration: [String: String]
+
+    let mode: ConfigurationMode
+    let baseURL: URL
+
+    init(bundle: Bundle = Bundle.main,
+         plistName: String = ConfigurationKeys.configurationFile,
+         mode: ConfigurationMode = .Staging) throws {
+        guard let resourcePath = bundle.path(forResource: plistName,
+                                             ofType: ConfigurationKeys.defaultFileType),
+            let baseConfiguration = NSDictionary(contentsOfFile: resourcePath) as? [String: Any],
+            let configuration = baseConfiguration[String(describing: mode)] as? [String: String] else { throw ConfigurationError.noConfigurationFile }
+
+        self.configuration = configuration
+        self.mode = mode
+        guard let baseURLString = configuration[ConfigurationKeys.baseURL], let baseURL = URL(string: baseURLString) else {
+            throw ConfigurationError.noBaseURL
+        }
+        self.baseURL = baseURL
+    }
+}
