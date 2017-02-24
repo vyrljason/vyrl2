e7514a6bc37b1e04a67ed9d8e63ecca229c5dcc2
Oktawian Chojnacki <oktawian@me.com>
2017-02-24 11:11:39 +0100

    72 - Update messages.

--- Vyrl/LeftMenuInteractorFactory.swift
+++ Vyrl/LeftMenuInteractorFactory.swift
@@ -10,9 +10,9 @@ enum LeftMenuInteractorFactory {
         static let titleAttributes: [String: Any] = [:] //This should be adjusted by the final design
         static let descriptionAttributes: [String: Any] = [:] //This should be adjusted by the final design
         static let noDataTitle = NSLocalizedString("No categories", comment: "")
+        static let noDataDescription = NSLocalizedString("Sorry, there are no categories at the moment.", comment: "")
         static let networkingErrorTitle = NSLocalizedString("Something went wrong", comment: "")
-        static let noDataDescription = NSLocalizedString("Sorry, there are no acegories at the moment.", comment: "")
-        static let networkingErrorDescription = NSLocalizedString("Pull down to refresh.", comment: "")
+        static let networkingErrorDescription = NSLocalizedString("Please contact the administrator.", comment: "")
     }
 
     static func make() -> LeftMenuInteractor {
