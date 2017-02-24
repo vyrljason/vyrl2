1333812fb374c3b060cee25b18e92de340cbb94c
Kamil Zietek <kamil.zietek@allinmobile.co>
2017-02-24 14:12:47 +0100

    37 - Added BrandStoreService tests, improved BrandService tests

--- VyrlTests/BrandStoreServiceTest.swift
+++ VyrlTests/BrandStoreServiceTest.swift
@@ -5,22 +5,20 @@
 @testable import Vyrl
 import XCTest
 
-final class BrandsServiceTests: XCTestCase {
-
-    var resource: BrandsResourceMock!
-    var service: Service<BrandsResourceMock>!
-    var subject: BrandsService!
-
+final class BrandStoreServiceTest: XCTestCase {
+    
+    var subject: BrandStoreService!
+    var resource: ProductsResourceMock!
+    
     override func setUp() {
         super.setUp()
-        resource = BrandsResourceMock(amount: 1)
-        service = Service<BrandsResourceMock>(resource: resource)
-        subject = BrandsService(resource: service)
+        resource = ProductsResourceMock(amount: 1)
+        let service = Service<ProductsResourceMock>(resource: resource)
+        subject = BrandStoreService(resource: service)
     }
-
-    func test_get_whenSuccess_returnsBrands() {
+    
+    func test_get_whenSuccess_returnsProducts() {
         resource.success = true
-
         var wasCalled = false
         subject.get { result in
             wasCalled = true
@@ -28,10 +26,9 @@ final class BrandsServiceTests: XCTestCase {
         }
         XCTAssertTrue(wasCalled)
     }
-
+    
     func test_get_whenFailure_returnsError() {
         resource.success = false
-
         var wasCalled = false
         subject.get { result in
             wasCalled = true
