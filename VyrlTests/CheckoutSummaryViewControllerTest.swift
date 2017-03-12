77907a69f9bb428c0ff8e74bf3411966484c0eed
Krzysztof Rodak <krodak.konta@gmail.com>
2017-03-12 18:22:55 +0100

    60 - Adding snapshot test

---
+++ VyrlTests/CheckoutSummaryViewControllerTest.swift
@@ -0,0 +1,28 @@
+//
+//  Copyright © 2017 Vyrl. All rights reserved.
+//
+
+@testable import Vyrl
+import XCTest
+
+final class CheckoutSummaryInteractorMock: CheckoutSummaryInteracting {
+    weak var navigation: CheckoutNavigationDismissing?
+    func didTapGoToCollabs() { }
+}
+
+final class CheckoutSummaryViewSnapshotTest: SnapshotTestCase {
+
+    override func setUp() {
+        super.setUp()
+
+        recordMode = false
+    }
+
+    func testViewCorrect() {
+        let interactor = CheckoutSummaryInteractorMock()
+        let view = CheckoutSummaryViewController(interactor: interactor)
+        let _ = view.view
+
+        verifyForScreens(view: view.view)
+    }
+}
