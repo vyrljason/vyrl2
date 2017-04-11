5714e532d05dc0261adc578c647e4ba430ab3d54
Wojciech Stasiński <wojtchas@gmail.com>
2017-04-11 14:02:18 +0200

    added basic account settings view

--- Vyrl/AccountViewController.swift
+++ Vyrl/AccountViewController.swift
@@ -4,6 +4,48 @@
 
 import UIKit
 
-final class AccountViewController: UIViewController {
+final class AccountViewController: UIViewController, HavingNib {
+    static var nibName: String = "AccountViewController"
+    
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var influencerNameLabel: UILabel!
+    @IBOutlet weak var headerView: UIView!
 
+    init() {
+        super.init(nibName: AccountViewController.nibName, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpHeaderView()
+    }
+    
+    fileprivate func setUpHeaderView() {
+        headerView.applyGradient(withColors: [.lightGray, .white], gradientOrientation: .vertical)
+    }
+    
+    @IBAction func didTapViewProfile(_ sender: Any) {
+    }
+    
+    @IBAction func didTapTermsOfService(_ sender: Any) {
+    }
+    
+    @IBAction func didTapFaq(_ sender: Any) {
+    }
+    
+    @IBAction func didTapFoundABug(_ sender: Any) {
+    }
+    
+    @IBAction func didTapDeleteAccount(_ sender: Any) {
+    }
+    
+    @IBAction func didSwitchEmailNotifications(_ sender: Any) {
+    }
+    
+    @IBAction func didSwitchPushNotifications(_ sender: Any) {
+    }
 }
---
+++ Vyrl/UIView+Gradients.swift
@@ -0,0 +1,41 @@
+//
+//  Copyright © 2017 Vyrl. All rights reserved.
+//
+
+import UIKit
+
+typealias GradientPoints = (startPoint: CGPoint, endPoint: CGPoint)
+
+enum GradientOrientation {
+    case horizontal
+    case vertical
+    
+    var startPoint: CGPoint {
+        return points.startPoint
+    }
+    
+    var endPoint: CGPoint {
+        return points.endPoint
+    }
+    
+    var points: GradientPoints {
+        switch self {
+        case .horizontal:
+            return (CGPoint(x: 0.0, y: 0.5), CGPoint(x: 1.0, y: 0.5))
+        case .vertical:
+            return (CGPoint(x: 0.0, y: 0.0), CGPoint(x: 0.0, y: 1.0))
+        }
+    }
+}
+
+extension UIView {
+    
+    func applyGradient(withColors colors: [UIColor], gradientOrientation orientation: GradientOrientation) {
+        let gradient = CAGradientLayer()
+        gradient.frame = self.bounds
+        gradient.colors = colors.map { $0.cgColor }
+        gradient.startPoint = orientation.startPoint
+        gradient.endPoint = orientation.endPoint
+        self.layer.insertSublayer(gradient, at: 0)
+    }
+}
